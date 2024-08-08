44e8b64b9b6094e6a9bdf958ce5885eb27986872
MaksimPerepeliuk <max_perepelyuk@mail.ru>
2024-08-08 09:43:24 +0300

    create new brain game - brain-progression

---
+++ brain_games/scripts/games/brain_progression.py
@@ -0,0 +1,28 @@
+from random import randint
+from brain_games.scripts.engine import engine
+
+
+def brain_progression():
+    progression_step = randint(1, 10)
+    start_num = randint(1, 30)
+    progression_len = 10
+
+    progression = list(range(start_num,
+                             start_num + (progression_step * progression_len),
+                             progression_step))
+
+    hidden_index = randint(0, len(progression) - 1)
+    right_answer = str(progression[hidden_index])
+
+    progression[hidden_index] = None
+    question = str(progression)[1:-1].replace('None', '..')
+
+    return question, right_answer
+
+
+def main():
+    engine(brain_progression)
+
+
+if __name__ == '__main__':
+    main()
